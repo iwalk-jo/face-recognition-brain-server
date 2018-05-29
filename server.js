12f84bcc2dfe884bac1f29940229316b88a774e8
iwalk <36207015+iwalk-jo@users.noreply.github.com>
2018-05-29 18:19:52 +0700

    testing database connection

--- server.js
+++ server.js
@@ -18,12 +18,9 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-sinuous-88759',
-		user: 'postgres',
-		password: '',
-		database: 'smart-brain-db'
+		host: process.env.DATABASE_URL,
+		ssl: true
 	}
-
 });
 
 const app = express();
