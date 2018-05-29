6cb914824bcc171212ab5883f577ae548c998c03
iwalk <36207015+iwalk-jo@users.noreply.github.com>
2018-05-29 21:11:42 +0700

    code clean up

--- controllers/image.js
+++ controllers/image.js
@@ -2,6 +2,7 @@ const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
 	apiKey: 'c0341bc371084fed95e9a5b12d6a68bd'
+	//apiKey: process.env.API_CLARIFAI
 });
 const handleApiCall = (req, res) => {
 	app.models
@@ -29,39 +30,3 @@ module.exports = {
 	handleImage: handleImage,
 	handleApiCall: handleApiCall
 }
-
-
-
-
-/*const Clarifai = require('clarifai');
-
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
-	apiKey: 'c0341bc371084fed95e9a5b12d6a68bd'
-});
-
-const handleApiCall = (req, res) => {
-  app.models
-  .predict( Clarifai.FACE_DETECT_MODEL, req.body.input)
-  .ther(data => {
-    res.json(data);
-  })
-  .catch(err => res.status(400).json('unable to work with API'))
-}
-
-const handleImage = (req, res, db) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-  }
-
-  module.exports = {
-      handleImage,
-      handleApiCall
-  };
-  */
